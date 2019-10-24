149d529c8e48706ca8be5c8f443ce9d758cde4be
cseiasp <caroline.seilern@hotmail.com>
2019-10-24 10:43:06 +0100

    Done.

--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,23 @@
 # Enter your object-oriented solution here!
+class Multiples
+
+    attr_reader :limit
+
+    def initialize(limit)
+        @limit = limit
+    end
+
+    def collect_multiples
+        
+        num_array = [*1...@limit]
+    
+        num_array.select do |num|
+            num % 3 == 0 || num % 5 == 0
+        end
+    end
+    
+    def sum_multiples
+        collect_multiples.sum
+    end
+    
+end
