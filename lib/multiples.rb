26881dc87ef44122583889ac60f27c5be85718c7
gortron <gordon.lanza@gmail.com>
2019-10-25 11:56:55 +0100

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,21 @@
 # Enter your procedural solution here!
+
+require 'pry'
+
+def collect_multiples(num)
+  i = 1
+  multiples = []
+  while i < num do
+    if i % 3 == 0
+      multiples.push(i)
+    elsif i % 5 == 0
+      multiples.push(i)
+    end
+    i += 1 
+  end
+  multiples
+end
+
+def sum_multiples(num)
+  collect_multiples(num).sum
+end
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,15 @@
 # Enter your object-oriented solution here!
+class Multiples
+  def initialize(num)
+    @num = num
+  end
+
+  def collect_multiples
+    Array(1..@num-1).select{|num| num % 3 == 0 || num % 5 == 0 }
+  end
+
+  def sum_multiples
+    collect_multiples.sum
+  end
+
+end
